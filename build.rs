9022a22fd299f35ba6c72f76aa644422ab225b82
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2026-02-19 01:18:39 +0900

    modified build.rs

--- build.rs
+++ build.rs
@@ -5,7 +5,18 @@ fn main() {
     let use_snmalloc = std::env::var("CARGO_FEATURE_USE_SNMALLOC").is_ok();
     let use_tcmalloc_static = std::env::var("CARGO_FEATURE_USE_TCMALLOC_STATIC").is_ok();
 
-    if target_os == "windows" && use_snmalloc {
+    if use_tcmalloc_static {
+        let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").expect("OUT_DIR was not set"));
+        let build_dir = out_dir.join("build");
+        println!("cargo:rustc-link-lib=static=tcmalloc");
+        println!("cargo:rustc-link-search=native={}/.libs", build_dir.display());
+        
+        let libs = ["stdc++", "unwind"];
+        for lib in libs {
+            println!("cargo:rustc-link-lib={}", lib);
+
+        }
+    } else if target_os == "windows" && use_snmalloc {
         // snmalloc
         println!("cargo:rustc-link-arg=/nodefaultlib:libucrt.lib");
 
@@ -13,25 +24,14 @@ fn main() {
         for lib in libs {
             println!("cargo:rustc-link-lib={}", lib);
         }
+    } else if target_os == "linux" && use_snmalloc {
+        println!("cargo:rustc-link-lib=static=snmallocshim-rust");
+        println!("cargo:rustc-link-lib=stdc++");
+        //println!("cargo:rustc-link-lib=atomic");
     } else if target_os == "windows" {
         // https://github.com/microsoft/mimalloc/blob/af21001f7a65eafb8fb16460b018ebf9d75e2ad8/CMakeLists.txt#L487
         let libs = ["psapi", "shell32", "user32", "advapi32", "bcrypt"];
         //let libs = ["advapi32"];
-        for lib in libs {
-            println!("cargo:rustc-link-lib={}", lib);
-        }
-    }
-    if use_tcmalloc_static {
-        let out_dir =
-            std::path::PathBuf::from(std::env::var_os("OUT_DIR").expect("OUT_DIR was not set"));
-        let build_dir = out_dir.join("build");
-        println!("cargo:rustc-link-lib=static=tcmalloc");
-        println!(
-            "cargo:rustc-link-search=native={}/.libs",
-            build_dir.display()
-        );
-
-        let libs = ["stdc++", "unwind"];
         for lib in libs {
             println!("cargo:rustc-link-lib={}", lib);
         }
